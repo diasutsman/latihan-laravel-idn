3966c06651f5a4fe0621f09c99100d53f276278c
Dias Utsman <utsmand91@gmail.com>
2023-01-19 08:03:57 +0700

    Make seeder and factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Dias Utsman',
+            'email' => 'utsmand91@gmail.com',
+            'password' => bcrypt('asdfghjkl')
+        ]);
     }
 }
